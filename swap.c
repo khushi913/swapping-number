60e73c2f8eddbb1f87915697793b900d3a4cc50e
khushi913 <72295764+khushi913@users.noreply.github.com>
2020-10-03 13:25:43 +0530

    swap.c
    project on swapping numbers

---
+++ swap.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+int x,y;
+  printf("enter number a =  ");
+  sacnf("%d", x);
+  printf("enter number b = ");
+  scanf("%d", y);
+  printf(" swaping number a = %d and b = %d", y ,x);
+  return 0;
+  
